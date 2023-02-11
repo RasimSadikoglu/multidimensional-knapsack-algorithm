94ea91c9a62509cabb3a6733bc541b34a6ef4297
Rasim Sadıkoğlu <sadikoglurasim@gmail.com>
2023-02-12 00:03:40 +0300

    dfs

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 run:
 	g++ -O3 -Wall -Wextra -o main main.cpp
-	./main sample3.txt
+	./main sample1.txt
--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ bool sortf(vector<int> v1, vector<int> v2);
 void handler(int num);
 void print_console(int stage, int max, int max_stage);
 
-bool terminate = false;
+volatile bool terminate = false;
 
 int main(int argc, char **argv) {
 
@@ -75,12 +75,6 @@ int main(int argc, char **argv) {
 }
 
 void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits) {
-    map<vector<int>, int> table;
-
-    vector<int> weights(limits);
-
-    weights.push_back(items.size() - 1);
-
     bool max_stack[items.size()] {};
     int result = knapsack_dfs(items, limits, max_stack);
 
@@ -96,95 +90,17 @@ void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits)
     return;
 }
 
-// Recursive DP
-int knapsack_dp(const vector<vector<int>> &items, map<vector<int>, int> &table, vector<int> &weights) {
-
-    int *val, item;
-    if ((item = weights.back()) == -1) return 0;
-
-    val = &table[weights];
-    vector<int> cweights(weights);
-    //weights.clear();
-
-    if (*val != 0) {
-        //std::cout << weights.back() << std::endl;
-        return *val == -1 ? 0 : *val;
-    }
-    
-    cweights[cweights.size() - 1]--;
-    int p1 = knapsack_dp(items, table, cweights);
-
-    for (size_t i = 0; i < cweights.size() - 1; i++) {
-        if (cweights[i] - items[item][i + 1] < 0) {
-            if (p1 == 0) *val = -1;
-            else *val = p1;
-            return p1;
-        }
-        cweights[i] -= items[item][i + 1];
-    }
-
-    int p2 = knapsack_dp(items, table, cweights) + items[item][0];
-
-    if (p2 >= p1) *val = p2;
-    else *val = p1;
-
-    if (*val == 0) *val = -1;
-
-    return *val;
-}
-
-// Iterative DP
-int knapsack_dpi(const vector<vector<int>> &items, map<vector<int>, int> &table, vector<int> weights) {
-    
-    std::stack<vector<int>> s;
-    s.push(weights);
-    do {
-        vector<int> current = s.top();
-
-        current[current.size() - 1]--;
-
-        int v1 = 0, v2 = 0;
-        if (current.back() == -1) {
-            v1 = 0;
-        } else if ((v1 = table[current]) == 0) {
-            s.push(current);
-            continue;
-        }
-
-        bool isv2 = true;
-        for (size_t i = 0; i < current.size() - 1; i++) {
-            current[i] -= items[current.back() + 1][i + 1];
-            if (current[i] < 0) {
-                isv2 = false;
-                break;
-            }
-        }
-        
-        if (isv2 && current.back() == -1) {
-            v2 = 0;
-        } else if (isv2 && (v2 = table[current]) == 0) {
-            s.push(current);
-            continue;
-        }
-
-        if (v1 == -1) v1 = 0;
-        if (v2 == -1) v2 = 0;
-
-        if (isv2) v2 += items[current.back() + 1][0];
-
-        int *val = &table[s.top()];
-        *val = v1;
-        if (isv2) *val = v2 > v1 ? v2 : v1;
-        if (*val == 0) *val = -1;
-
-        s.pop();
-
-    } while (!s.empty());
-
-    return table[weights];
-}
-
 int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_stack[]) {
+
+    for (size_t i = 0; i < items.size(); i++) {
+        float val = 0;
+        for (size_t j = 0; j < limits.size(); j++) {
+            val += (float)items[i][j + 1] / limits[j];
+        }
+        val /= limits.size();
+        val = items[i][0] / val;
+        items[i].push_back(val);
+    }
 
     for (size_t i = 0; i < items.size(); i++) items[i].push_back(i);
     std::sort(items.begin(), items.end(), sortf);
@@ -202,6 +118,8 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
     int si_min = si + 1;
 
     int value = 0;
+    bool prev = 0;
+
     while (si != -1 && !terminate) {
 
         bool top = stack[si];
@@ -213,11 +131,11 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
                 si_min = si;
                 print_console(si_min, max_value, items_size);
             }
+            prev = top;
+            stack[si] = 0;
             si--;
             continue;
         }
-
-        stack[si] = 1;
 
         int tw[limits_size];
         memcpy(tw, weights, sizeof(int) * limits_size);
@@ -229,21 +147,27 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
         }
 
         if (!isfit) {
-            si--;
+            if (!prev) si = items_size - 1;
+            else si--;
+            prev = 1;
             continue;
         }
+
+        stack[si] = 1;
 
         value += items[si][0];
         memcpy(weights, tw, sizeof(int) * limits_size);
 
-        for (si++; si < items_size; si++) stack[si] = 0;
-        si--;
-
-        if (value >= max_value) {
+        if (value > max_value) {
             max_value = value;
             memcpy(max_stack, stack, items_size * sizeof(bool));
             print_console(si_min, max_value, items_size);
         }
+
+        if (prev) si = items_size - 1;
+        else si--;
+
+        prev = top;
     }
 
     for (size_t i = 0; i < items.size(); i++) items[i].push_back(max_stack[i]);
@@ -261,6 +185,7 @@ bool sortf(vector<int> v1, vector<int> v2) {
 void handler(int num) {
     std::cout << "Saving current results...\n";
     terminate = true;
+    return;
 }
 
 void print_console(int stage, int max, int max_stage) {
